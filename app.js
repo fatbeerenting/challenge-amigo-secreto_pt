8862db40a7520ac93a8e40b09dd7610e75968da1
Fat Bee <fat.bee.renting@gmail.com>
2025-02-24 23:03:19 -0300

    meu segundo commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação.
 // Aqui você deverá desenvolver a lógica para resolver o problema.
 
+//Variavel criada durante a execucao do desafio!
 let amigos = [];
